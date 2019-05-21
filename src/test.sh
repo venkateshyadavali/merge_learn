2c8efdaabf6d973e79a0697eb04ce6c1294a35f6
venkatesh <venkatesh.yadavali@gmail.com>
2019-05-21 14:49:14 +0530

    changes on src/test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,3 @@
 File created in testing branch
 Second line on test branch to see if git pull brings these changes to local repo on pc.
+Checking if line would exist in the working tree, if git pull is done. add and commit are not performed.
